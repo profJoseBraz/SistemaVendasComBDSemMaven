f0c1eca0abbdc921a2806360662c99db94658a12
profjosebraz <prof.josebrazz@gmail.com>
2024-05-09 11:59:31 -0300

    alt pasta adm

--- src/com/mycompany/visao/outros/administrativo/MenuPrincipal.java
+++ src/com/mycompany/visao/outros/administrativo/MenuPrincipal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.visao.outros.corporativo;
+package com.mycompany.visao.outros.administrativo;
 
 import com.mycompany.utilizades.BancoDeDadosMySql;
 import com.mycompany.utilizades.Formularios;
